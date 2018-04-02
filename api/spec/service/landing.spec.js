d901a56ed241a8620388a2b77e6e868ba33bb389
dopatraman <prakash@asanet.com>
2018-04-01 22:59:44 -0700

    unnecessary tests

--- api/spec/service/landing.spec.js
+++ api/spec/service/landing.spec.js
@@ -1,21 +1,19 @@
-var LandingPageService = require("../../service/landing");
-
 describe("LandingPageService", () => {
     var svc;
     var mockDb;
     beforeEach(() => {
-        mockDb = jasmine.createSpyObj('dbSvc', ['query']);
-        svc = new LandingPageService(mockDb);
+        // mockDb = jasmine.createSpyObj('dbSvc', ['query']);
+        // svc = new LandingPageService(mockDb);
     });
 
-    describe('#insertSubmission', () => {
+    xdescribe('#insertSubmission', () => {
         it('should call a db query', () => {
             svc.insertSubmission("prakash@carbonfive.com");
             expect(mockDb.query).toHaveBeenCalled();
         });
     });
 
-    describe('#fetchSubmissions', () => {
+    xdescribe('#fetchSubmissions', () => {
         it('should make a db query', () => {
             svc.fetchSubmissions();
             expect(mockDb.query).toHaveBeenCalled();
